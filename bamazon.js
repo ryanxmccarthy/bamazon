19ac6e38c9f8c506f62243f8c75ea5799426f2b8
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-11-14 00:37:24 -0500

    added cli-table

--- bamazon.js
+++ bamazon.js
@@ -1,4 +1,25 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var table = require('cli-table');
 
-console.log('were in')
+var con = mysql.createConnection({
+	host: "localhost",
+  	user: "root",
+  	password: "",
+  	database: "bamazon"
+});
+
+con.connect(function(err) {
+  	if (err) throw err;
+  	console.log("Connected as id " + con.threadId);
+  	afterConnect();
+  	con.end();
+});
+
+function afterConnect() {
+	con.query('SELECT * FROM products', function(err, results){
+		if (err) throw err;
+		console.log(results)
+		console.log('====================================')
+	})
+}
